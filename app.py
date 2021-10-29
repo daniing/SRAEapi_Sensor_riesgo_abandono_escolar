ca3a9b20471e4d21d7972feee8d294344ad2fe6f
daniing <36769686+daniing@users.noreply.github.com>
2021-10-28 19:42:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,23 +13,7 @@ from sklearn.preprocessing import StandardScaler
 # Create flask app
 app = Flask(__name__)
 
-#filename_model_d = 'model_api_demog.pkl'
-#filename_model_md = 'model_api_md.pkl'
-#filename_model_a = 'model_api_a.pkl'
-#filename_model_ma = 'model_api_ma.pkl'
-#filename_latefusion = 'model_latefusion.pkl'
-#filename_MLP = 'model_MLP.pkl'
-#model_d = joblib.load(filename_model_d)
-#model_md = joblib.load(filename_model_md)
-#model_a = joblib.load(filename_model_a)
-#model_ma = joblib.load(filename_model_ma)
-#model_lf = joblib.load(filename_latefusion)
-#model_MLP = joblib.load(filename_MLP)
-
 model_MLP = pickle.load(open('model_MLP.pkl','rb'))
-
-#with open(filename_MLP, 'rb') as f: 
-#    model_MLP = pickle.loads(f.read())
     
 @app.route("/")
 def Home():
@@ -38,40 +22,31 @@ def Home():
 @app.route("/predict", methods = ["GET","POST"])
 def predict():
     
-    #features_ini = [str(x) for x in request.form.values()]
-    #features_ini_ = np.array(features_ini)
-    #features = np.array(features_ini)
-    #features_float = features[1::]
-    #features_float_ = [float(x) for x in features_float]
-    #feature_date = features[:1:]
-    #fech_n = request.form.get('fecha_nacimiento')
-    #fech_n_ = " ".join(str(x) for x in feature_date)
-    fech_n_ = request.form['edad']
+    fech_n_ = request.form.get('edad')
     
     fech1 = datetime.strptime(fech_n_, '%Y-%m-%d').date()
     fech2 = date.today()# criterio de medición (año) 
     edad_ = round((fech2-fech1)/dt.timedelta(365,5,49,12),3) #promedio de años comunes y bisiestos 
     
-    feature_hc = request.form['hermanos_Colegio']
-    feature_g = request.form['gender']
-    feature_grado_a = request.form['curso']
-    feature_score_sisben = request.form['puntaje_sisben']
-    feature_distance = request.form['distancia']
-    feature_year_in = request.form['year_in']
-    feature_grado_in = request.form['grado_in']
-    feature_math = request.form['math']
-    feature_cn = request.form['cn']
-    feature_ing = request.form['ing']
-    feature_esp = request.form['esp']
-    feature_catp = request.form['cp']
-    feature_soc = request.form['soc']
-    feature_rel = request.form['rel']
-    feature_art = request.form['art']
-    feature_edf = request.form['edf']
-    feature_tec = request.form['tech']
-    feature_emp = request.form['emp']
-    feature_etv = request.form['etv']
-    
+    feature_hc = request.form.get('hermanos_Colegio')
+    feature_g = request.form.get('gender')
+    feature_grado_a = request.form.get('curso')
+    feature_score_sisben = request.form.get('puntaje_sisben')
+    feature_distance = request.form.get('distancia')
+    feature_year_in = request.form.get('year_in')
+    feature_grado_in = request.form.get('grado_in')
+    feature_math = request.form.get('math')
+    feature_cn = request.form.get('cn')
+    feature_ing = request.form.get('ing')
+    feature_esp = request.form.get('esp')
+    feature_catp = request.form.get('cp')
+    feature_soc = request.form.get('soc')
+    feature_rel = request.form.get('rel')
+    feature_art = request.form.get('art')
+    feature_edf = request.form.get('edf')
+    feature_tec = request.form.get('tech')
+    feature_emp = request.form.get('emp')
+    feature_etv = request.form.get('etv')
     
     list_fini = [feature_hc,feature_g,feature_grado_a,feature_score_sisben,
                  feature_distance,feature_year_in,feature_grado_in,
